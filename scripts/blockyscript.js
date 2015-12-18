6baa0ff5ae6d725bc07b9295858e054fffe88a3b
RaviKumarBH <bhchinna578@gmail.com>
2015-12-18 14:36:57 +0530

    My Commit to blockyscript

--- scripts/blockyscript.js
+++ scripts/blockyscript.js
@@ -3,5 +3,5 @@
 write=nobody
 execute=authenticated 
   **/ 
- /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="56" inline="false" x="-9" y="49"><value name="return"><block type="text" id="96"><field name="TEXT">my messsssssss</field></block></value></block></xml>*#*#*/
-return 'my messsssssss'			
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="56" inline="false" x="-9" y="49"><value name="return"><block type="text" id="96"><field name="TEXT"></field></block></value></block></xml>*#*#*/
+return ''			
