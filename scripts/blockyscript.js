e30e2c858e0d4c8df213273d154df5f61072891b
RaviKumarBH <bhchinna578@gmail.com>
2015-12-18 13:29:24 +0530

    my commit

---
+++ scripts/blockyscript.js
@@ -0,0 +1,7 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="56" inline="false" x="-9" y="49"><value name="return"><block type="text" id="96"><field name="TEXT">my messsssssss</field></block></value></block></xml>*#*#*/
+return 'my messsssssss'			
