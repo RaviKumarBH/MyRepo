a1d6990eb1919e726a6b62edf4ac9955d4c1ec65
RaviKumarBH <bhchinna578@gmail.com>
2016-01-27 18:07:47 +0530

    sdfsd

--- scripts/GitDiff/FirstScript.js
+++ scripts/GitDiff/FirstScript.js
@@ -3,104 +3,26 @@
 write=nobody
 execute=anonymous 
   **/ 
- return "my gitdiff feature1";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature2";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature3";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature4";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature5";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature6";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature7";
-return "my gitdiff feature";
-return "my gitdiff feature8";
-return "my gitdiff featur9e";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature10";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature11";
-return "my gitdiff feature";
-return "my gitdiff feature15";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature12";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature16";return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature17";
-return "my gitdiff feature";
-return "my gitdiff feature18";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature19";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature20";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
-return "my gitdiff feature";
+ var fb = require("facebook");
+
+var currentDate = new Date();
+var formattedDate = currentDate.getFullYear() + "-" + atLeastTwoDigits(currentDate.getMonth() + 1)
+  + "-" + atLeastTwoDigits(currentDate.getDate()) + "T" + atLeastTwoDigits(currentDate.getHours())
+  + ":" + atLeastTwoDigits(currentDate.getMinutes()) + ":" + atLeastTwoDigits(currentDate.getSeconds());
+formattedDate += currentDate.getTimezoneOffset() < 0 ? "+" : "-";
+formattedDate += atLeastTwoDigits((Math.abs(currentDate.getTimezoneOffset()) / 60)) + "00";
+
+var message = "Check out this imagi: http://www.google.com/intl/en_ALL/images/logo.gif";
+
+var credentials = {
+  "apiKey": "341726685895718",
+  "apiSecret": "62d55f6f76433f42c644ef2e9caf91bf",
+  "accessToken": "CAAE2zHG5HCYBAKnvZCb0e9EwIZAGqNhqDODvLfHHZChOZCkShI0kfFfolUPhaWGZBurLV1LpVuXLJJxb7ZCyqcdGTrwvhzcpbUQZAqNprl6bXMcQXFMEjRnahMeDYACdreEH7BpUtPZAkizyykqGu6uNaHM25d5AM3uigS7d84F7ZBmNsAoSD1Upbq0PbUiA34RdxAN2EqZALDJ6ZAPofHdzOQm"
+};
+
+return fb.post(message, credentials);
 
 
-			
+function atLeastTwoDigits (value) {
+    return value < 10 ? "0" + value : value;
+}			
