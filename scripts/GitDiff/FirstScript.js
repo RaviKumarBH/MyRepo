836b87c870526ae57e995250d40c8d829962d47e
RaviKumarBH <bhchinna578@gmail.com>
2016-01-27 18:11:38 +0530

    dfgsdfsd

--- scripts/GitDiff/FirstScript.js
+++ scripts/GitDiff/FirstScript.js
@@ -1,28 +1,70 @@
 /** Script ACLs do not delete 
  read=nobody 
 write=nobody
-execute=anonymous 
+execute=authenticated 
   **/ 
- var fb = require("facebook");
+ /* This is an example of posting a video to Facebook. */
 
-var currentDate = new Date();
-var formattedDate = currentDate.getFullYear() + "-" + atLeastTwoDigits(currentDate.getMonth() + 1)
-  + "-" + atLeastTwoDigits(currentDate.getDate()) + "T" + atLeastTwoDigits(currentDate.getHours())
-  + ":" + atLeastTwoDigits(currentDate.getMinutes()) + ":" + atLeastTwoDigits(currentDate.getSeconds());
-formattedDate += currentDate.getTimezoneOffset() < 0 ? "+" : "-";
-formattedDate += atLeastTwoDigits((Math.abs(currentDate.getTimezoneOffset()) / 60)) + "00";
+var fb = require("facebook");
 
-var message = "Check out this imagi: http://www.google.com/intl/en_ALL/images/logo.gif";
+//Define the resource URL to call
+var url = "https://graph.facebook.com/me/feed";
 
+//Define additional parmameters to pass
+var params = {"message": "some msg to post from Julz", "link" : "http://www.google.com"};
+
+//Define the HTTP Request Method to use
+var method = "POST";
+
+//Define the credentials
 var credentials = {
   "apiKey": "341726685895718",
   "apiSecret": "62d55f6f76433f42c644ef2e9caf91bf",
   "accessToken": "CAAE2zHG5HCYBAKnvZCb0e9EwIZAGqNhqDODvLfHHZChOZCkShI0kfFfolUPhaWGZBurLV1LpVuXLJJxb7ZCyqcdGTrwvhzcpbUQZAqNprl6bXMcQXFMEjRnahMeDYACdreEH7BpUtPZAkizyykqGu6uNaHM25d5AM3uigS7d84F7ZBmNsAoSD1Upbq0PbUiA34RdxAN2EqZALDJ6ZAPofHdzOQm"
 };
 
-return fb.post(message, credentials);
+return fb.callApi(url, method, params);
 
 
-function atLeastTwoDigits (value) {
-    return value < 10 ? "0" + value : value;
-}			
+/* This is an example of posting a video to Facebook. */
+
+var fb = require("facebook");
+
+//Define the resource URL to call
+var url = "https://graph.facebook.com/me/feed";
+
+//Define additional parmameters to pass
+var params = {"message": "some msg to post from Julz", "link" : "http://www.google.com"};
+
+//Define the HTTP Request Method to use
+var method = "POST";
+
+//Define the credentials
+var credentials = {
+  "apiKey": "341726685895718",
+  "apiSecret": "62d55f6f76433f42c644ef2e9caf91bf",
+  "accessToken": "CAAE2zHG5HCYBAKnvZCb0e9EwIZAGqNhqDODvLfHHZChOZCkShI0kfFfolUPhaWGZBurLV1LpVuXLJJxb7ZCyqcdGTrwvhzcpbUQZAqNprl6bXMcQXFMEjRnahMeDYACdreEH7BpUtPZAkizyykqGu6uNaHM25d5AM3uigS7d84F7ZBmNsAoSD1Upbq0PbUiA34RdxAN2EqZALDJ6ZAPofHdzOQm"
+};
+
+return fb.callApi(url, method, params);
+/* This is an example of posting a video to Facebook. */
+
+var fb = require("facebook");
+
+//Define the resource URL to call
+var url = "https://graph.facebook.com/me/feed";
+
+//Define additional parmameters to pass
+var params = {"message": "some msg to post from Julz", "link" : "http://www.google.com"};
+
+//Define the HTTP Request Method to use
+var method = "POST";
+
+//Define the credentials
+var credentials = {
+  "apiKey": "341726685895718",
+  "apiSecret": "62d55f6f76433f42c644ef2e9caf91bf",
+  "accessToken": "CAAE2zHG5HCYBAKnvZCb0e9EwIZAGqNhqDODvLfHHZChOZCkShI0kfFfolUPhaWGZBurLV1LpVuXLJJxb7ZCyqcdGTrwvhzcpbUQZAqNprl6bXMcQXFMEjRnahMeDYACdreEH7BpUtPZAkizyykqGu6uNaHM25d5AM3uigS7d84F7ZBmNsAoSD1Upbq0PbUiA34RdxAN2EqZALDJ6ZAPofHdzOQm"
+};
+
+return fb.callApi(url, method, params);			
