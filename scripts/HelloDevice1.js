64c58672ec0d3e0509ab6b388fbde7dc7c66b567
RaviKumarBH <bhchinna578@gmail.com>
2016-03-03 11:25:21 +0530

    wetwer

--- scripts/HelloDevice1.js
+++ scripts/HelloDevice1.js
@@ -49,4 +49,5 @@ if(storage.local.theName != null) scriptResponse += " " + storage.local.theName;
 if(country != null) scriptResponse += " from " + country;
 scriptResponse += "!";
 
-return scriptResponse;			
+return scriptResponse;
+//test			
