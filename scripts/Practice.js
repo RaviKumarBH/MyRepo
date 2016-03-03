a1d5ee6071ff09e5749ac8394487af2414a5603a
RaviKumarBH <bhchinna578@gmail.com>
2016-03-03 11:46:46 +0530

    test

--- scripts/Practice.js
+++ scripts/Practice.js
@@ -3,8 +3,13 @@
 write=nobody
 execute=authenticated 
   **/ 
- 
  var arrayDevices = ["My Msg1","My Msg12","My Msg123","My Msg1234","My Msg5","My Msg6"];
+console.log(arrayDevices.length);
+console.log(arrayDevices.pop());
+console.log(arrayDevices.indexOf("My Msg12"));
+console.log(arrayDevices.length);
+console.log(arrayDevices.reverse());
+var arrayDevices = ["My Msg1","My Msg12","My Msg123","My Msg1234","My Msg5","My Msg6"];
 console.log(arrayDevices.length);
 console.log(arrayDevices.pop());
 console.log(arrayDevices.indexOf("My Msg12"));
